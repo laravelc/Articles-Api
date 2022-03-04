3afd452f2f3a26b6d013a515c68bfcc7aec78430
aleksey <av.pavlow@gmail.com>
2022-03-04 21:35:58 +0300

    Логгирование запроса

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * Listener каждого SQL-запроса. Нужно чтобы узнать сколько времени выполняется запрос
+         *
+         */
+        DB::listen(function ($query) {
+            $query->sql; // выполненная sql-строка
+            $query->bindings; // параметры, переданные в запрос (то, что подменяет '?' в sql-строке)
+            $query->time; // время выполнения запроса
+        });
     }
 }
